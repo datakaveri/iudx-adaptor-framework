151c2219a1b1e732d4aa3ebc926d4961cbe79acc
rraks <rakshitadmar@gmail.com>
2021-03-09 23:57:57 +0530

    Improved setup to include compose for flink

--- setup/start_local_dev_env.sh
+++ setup/start_local_dev_env.sh
@@ -6,4 +6,5 @@ docker network create adaptor-net
 ./setup/scripts/start_monitoring_service.sh
 docker-compose -f ./setup/rmq/docker-compose.yml up -d
 docker-compose -f ./setup/postgres/docker-compose.yml up -d
-# docker-compose -f ./setup/mockserver/docker-compose.yml up -d
+docker-compose -f ./setup/mockserver/docker-compose.yml up -d
+docker-compose -f ./setup/flink/docker-compose.yml up -d
--- setup/stop_local_dev_env.sh
+++ setup/stop_local_dev_env.sh
@@ -3,7 +3,9 @@
 # To be executed from project root
 
 ./setup/scripts/stop_monitoring_service.sh
+./setup/flink/kill_local.sh
 docker-compose -f ./setup/rmq/docker-compose.yml down
 docker-compose -f ./setup/postgres/docker-compose.yml down
-# docker-compose -f ./setup/mockserver/docker-compose.yml down
+docker-compose -f ./setup/mockserver/docker-compose.yml down
+docker-compose -f ./setup/flink/docker-compose.yml down
 docker network rm adaptor-net
