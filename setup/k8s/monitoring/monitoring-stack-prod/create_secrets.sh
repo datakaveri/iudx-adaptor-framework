c30943260b4d1e9c67934af3a0e4533edb13da91
swarup-e <swarup@datakaveri.org>
2022-06-15 11:50:03 +0530

    Removed unused monitoring code and added prometheus config

--- setup/k8s/monitoring/monitoring-stack-prod/create_secrets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e 
-echo -n  $(cat /dev/urandom |  tr -dc 'a-zA-Z0-9-!@#$%^&*()+{}|:<>?=' | head -c 30) > secrets/admin-user
-echo -n  $(cat /dev/urandom |  tr -dc 'a-zA-Z0-9-!@#$%^&*()+{}|:<>?=' | head -c 30) > secrets/admin-password
-
