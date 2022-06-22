5c5c9c21c7bb749bdbc2ff5829f43ec2193d466c
swarup-e <swarup@datakaveri.org>
2022-06-22 11:49:45 +0530

    Updated jackson test case

--- framework/src/test/java/in/org/iudx/adaptor/utils/JacksonTest.java
+++ framework/src/test/java/in/org/iudx/adaptor/utils/JacksonTest.java
@@ -53,5 +53,34 @@ public class JacksonTest {
             System.out.println("Field name: " + fieldName + " Field value +" + fieldValue);
         }
     }
+
+
+    @Test
+    void testJacksonStreamingFirstToken() throws  InterruptedException, IOException {
+//        String json = "[{\"key\":123,\"body\":\"Test1\"}, " + "{\"key\":456,\"body\":\"Test2\"}]";
+        String json = "{\"deviceId\":\"abc-456\",\"k1\":530,\"time\":\"2022-06-22T05:59:57Z\"}";
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        JsonFactory jfactory = new JsonFactory();
+        JsonParser jsonParser = jfactory.createParser(json);
+
+        JsonToken firstToken = jsonParser.nextToken();
+        if (firstToken == JsonToken.START_ARRAY) {
+            while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
+                jsonParser.nextToken();
+                Message data = mapper.readValue(jsonParser, Message.class);
+                System.out.println(data);
+                System.out.println(data.getEventTime());
+            }
+        }
+
+        if (firstToken == JsonToken.START_OBJECT) {
+            Message data = mapper.readValue(jsonParser, Message.class);
+            System.out.println(data);
+            System.out.println(data.getEventTime());
+        }
+    }
 }
 
