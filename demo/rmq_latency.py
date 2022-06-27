f2225b7fa77902dfe91e84e9f68c020e5d2cea40
swarup-e <swarup@datakaveri.org>
2022-06-27 16:51:19 +0530

    RMQ latency test script

---
+++ demo/rmq_latency.py
@@ -0,0 +1,35 @@
+from asyncio import current_task
+import pika, sys, os, json
+from dateutil import parser
+import time
+from datetime import datetime
+
+routing_key = sys.argv[1]
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+
+    def callback(ch, method, properties, body):
+        if routing_key == method.routing_key:
+            data = json.loads(body.decode())
+            current_time = time.time()
+            obsereved_time = datetime.strptime(data['observationDateTime'], "%Y-%m-%dT%H:%M:%S.%fZ").timestamp()
+            print(current_time - obsereved_time)
+            
+
+    channel.basic_consume(queue='adaptor-test', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
