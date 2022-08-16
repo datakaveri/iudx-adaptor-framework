4cc1d39fbf12ee755677c802f05d922cf4faf4cf
swarup-e <swarup@datakaveri.org>
2022-08-16 18:27:42 +0530

    removed jar files from lfs and created script to download

---
+++ setup/k8s/flink/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FLINK_VERSION=1.15.1
+SCALA_VERSION=2.12
+
+wget https://dlcdn.apache.org/flink/flink-${FLINK_VERSION}/flink-${FLINK_VERSION}-bin-scala_${SCALA_VERSION}.tgz -O flink.tgz
+
+
+tar -xf flink.tgz -C .
+mv flink-${FLINK_VERSION}/* .
+cp flink-conf.yaml conf/
+cp log4j-console.properties conf/
+rm -rf flink.tgz
+rm -rf flink-${FLINK_VERSION}
