f2bac0e0468a9b0694cc82515af552193a6588b7
Rakshit Ramesh <rakshitadmar@gmail.com>
2022-08-25 17:54:15 +0530

    [Test] Initial test harness based tests

--- framework/src/main/java/in/org/iudx/adaptor/process/RuleFunction.java
+++ framework/src/main/java/in/org/iudx/adaptor/process/RuleFunction.java
@@ -32,102 +32,106 @@ import java.util.Properties;
 
 public class RuleFunction extends KeyedBroadcastProcessFunction<String, Message, Rule, String> {
 
-    private static int EXPIRY_TIME = Integer.MIN_VALUE;
-    private final ListStateDescriptor<LinkedHashMap<String, Object>> listStateDescriptor = new ListStateDescriptor<>("listState", TypeInformation.of(new TypeHint<LinkedHashMap<String, Object>>() {
-    }));
-    private transient ListState<LinkedHashMap<String, Object>> listState;
-    private transient CalciteConnection calciteConnection;
+  private static int EXPIRY_TIME = Integer.MIN_VALUE;
+  private ListStateDescriptor<LinkedHashMap<String, Object>> listStateDescriptor;
 
-    @Override
-    public void open(Configuration parameters) throws ClassNotFoundException, SQLException {
+  private transient ListState<LinkedHashMap<String, Object>> listState;
+  private transient CalciteConnection calciteConnection;
+
+  @Override
+  public void open(Configuration parameters) throws ClassNotFoundException, SQLException {
+
+    Properties info = new Properties();
+    info.setProperty("lex", "JAVA");
+    Connection connection = DriverManager.getConnection("jdbc:calcite:", info);
+    calciteConnection = connection.unwrap(CalciteConnection.class);
+
+    listStateDescriptor = new ListStateDescriptor<>("listState", TypeInformation.of(new TypeHint<LinkedHashMap<String, Object>>(){}));
+    listState = getRuntimeContext().getListState(listStateDescriptor);
+  }
+
+  @Override
+  public void processElement(Message message, KeyedBroadcastProcessFunction<String, Message, Rule, String>.ReadOnlyContext readOnlyContext, Collector<String> collector) throws Exception {
+
+    ReadOnlyBroadcastState<Integer, Rule> ruleState = readOnlyContext.getBroadcastState(RuleStateDescriptor.ruleMapStateDescriptor);
+
+    SchemaPlus rootSchema = calciteConnection.getRootSchema();
+
+    LinkedHashMap<String,Object> obj = new JsonFlatten(new ObjectMapper().readTree(message.toString())).flatten();
+    listState.add(new JsonFlatten(new ObjectMapper().readTree(message.toString())).flatten());
+    Iterator<LinkedHashMap<String, Object>> iterator = listState.get().iterator();
+    List<LinkedHashMap<String, Object>> list = IteratorUtils.toList(iterator);
+
+    System.out.println(ruleState.get(1).sqlQuery);
+    System.out.println(list.get(0).get("a"));
+
+    Schema schema = new Schema();
+    schema.setData(list);
+    rootSchema.add("listState", schema);
 
 
-        Properties info = new Properties();
-        info.setProperty("lex", "JAVA");
-        Connection connection = DriverManager.getConnection("jdbc:calcite:", info);
-        calciteConnection = connection.unwrap(CalciteConnection.class);
 
-        listState = getRuntimeContext().getListState(listStateDescriptor);
+    try (Statement statement = calciteConnection.createStatement()) {
+      String sql = "select * from listState.state";
+      ResultSet rs = statement.executeQuery(sql);
 
+      while (rs.next()) {
+        String result = rs.getString("a");
+        collector.collect(String.valueOf(result));
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  @Override
+  public void processBroadcastElement(Rule rule, KeyedBroadcastProcessFunction<String, Message, Rule, String>.Context context, Collector<String> collector) throws Exception {
+    BroadcastState<Integer, Rule> broadcastState = context.getBroadcastState(RuleStateDescriptor.ruleMapStateDescriptor);
+    broadcastState.put(rule.ruleId, rule);
+    updateExpiryTime(rule, broadcastState);
+  }
+
+
+  @Override
+  public void onTimer(final long timestamp, final OnTimerContext ctx, final Collector<String> collector) throws Exception {
+
+    Rule rule = ctx.getBroadcastState(RuleStateDescriptor.ruleMapStateDescriptor).get(EXPIRY_TIME);
+
+    Optional<Long> cleanupEventTimeWindow = Optional.ofNullable(rule).map(Rule::getWindowMillis);
+    Optional<Long> cleanupEventTimeThreshold = cleanupEventTimeWindow.map(window -> timestamp - window);
+
+    cleanupEventTimeThreshold.ifPresent(this::removeElementFromState);
+  }
+
+  private void updateExpiryTime(Rule rule, BroadcastState<Integer, Rule> broadcastState) throws Exception {
+    Rule widestWindowRule = broadcastState.get(EXPIRY_TIME);
+
+
+    if (widestWindowRule == null) {
+      broadcastState.put(EXPIRY_TIME, rule);
+      return;
     }
 
-    @Override
-    public void processElement(Message message, KeyedBroadcastProcessFunction<String, Message, Rule, String>.ReadOnlyContext readOnlyContext, Collector<String> collector) throws Exception {
+    if (widestWindowRule.getWindowMillis() < rule.getWindowMillis()) {
+      broadcastState.put(EXPIRY_TIME, rule);
+    }
+  }
 
-        ReadOnlyBroadcastState<Integer, Rule> ruleState = readOnlyContext.getBroadcastState(RuleStateDescriptor.ruleMapStateDescriptor);
-
-        listState.add(new JsonFlatten(new ObjectMapper().readTree(message.toString())).flatten());
-
-        SchemaPlus rootSchema = calciteConnection.getRootSchema();
-        Iterator<LinkedHashMap<String, Object>> iterator = listState.get().iterator();
-        List<LinkedHashMap<String, Object>> list = IteratorUtils.toList(iterator);
-
-        Schema schema = new Schema();
-        schema.setData(list);
-        rootSchema.add("listState", schema);
-
-        try (Statement statement = calciteConnection.createStatement()) {
-            String sql = "select * from listState.state";
-            ResultSet rs = statement.executeQuery(sql);
-
-            while (rs.next()) {
-                String result = rs.getString("index");
-                collector.collect(String.valueOf(result));
-            }
-        } catch (Exception e) {
-            System.out.println(e);
+  private void removeElementFromState(Long threshold) {
+    try {
+      Iterator<LinkedHashMap<String, Object>> iterator = listState.get().iterator();
+      while (iterator.hasNext()) {
+        LinkedHashMap<String, Object> obj = iterator.next();
+        long eventTime = (long) obj.get("observationDateTime");
+        if (eventTime < threshold) {
+          iterator.remove();
         }
+      }
+      List<LinkedHashMap<String, Object>> list = IteratorUtils.toList(iterator);
+      listState.update(list);
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
     }
-
-    @Override
-    public void processBroadcastElement(Rule rule, KeyedBroadcastProcessFunction<String, Message, Rule, String>.Context context, Collector<String> collector) throws Exception {
-        BroadcastState<Integer, Rule> broadcastState = context.getBroadcastState(RuleStateDescriptor.ruleMapStateDescriptor);
-        broadcastState.put(rule.ruleId, rule);
-
-        updateExpiryTime(rule, broadcastState);
-    }
-
-
-    @Override
-    public void onTimer(final long timestamp, final OnTimerContext ctx, final Collector<String> collector) throws Exception {
-
-        Rule rule = ctx.getBroadcastState(RuleStateDescriptor.ruleMapStateDescriptor).get(EXPIRY_TIME);
-
-        Optional<Long> cleanupEventTimeWindow = Optional.ofNullable(rule).map(Rule::getWindowMillis);
-        Optional<Long> cleanupEventTimeThreshold = cleanupEventTimeWindow.map(window -> timestamp - window);
-
-        cleanupEventTimeThreshold.ifPresent(this::removeElementFromState);
-    }
-
-    private void updateExpiryTime(Rule rule, BroadcastState<Integer, Rule> broadcastState) throws Exception {
-        Rule widestWindowRule = broadcastState.get(EXPIRY_TIME);
-
-
-        if (widestWindowRule == null) {
-            broadcastState.put(EXPIRY_TIME, rule);
-            return;
-        }
-
-        if (widestWindowRule.getWindowMillis() < rule.getWindowMillis()) {
-            broadcastState.put(EXPIRY_TIME, rule);
-        }
-    }
-
-    private void removeElementFromState(Long threshold) {
-        try {
-            Iterator<LinkedHashMap<String, Object>> iterator = listState.get().iterator();
-            while (iterator.hasNext()) {
-                LinkedHashMap<String, Object> obj = iterator.next();
-                long eventTime = (long) obj.get("observationDateTime");
-                if (eventTime < threshold) {
-                    iterator.remove();
-                }
-            }
-            List<LinkedHashMap<String, Object>> list = IteratorUtils.toList(iterator);
-            listState.update(list);
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+  }
 
 }
---
+++ framework/src/test/java/in/org/iudx/adaptor/process/RuleFunctionTest.java
@@ -0,0 +1,131 @@
+package in.org.iudx.adaptor.process;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.Comparator;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.function.Function;
+import org.apache.flink.shaded.guava30.com.google.common.collect.ImmutableList;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import in.org.iudx.adaptor.sql.JsonArrayListTable;
+import in.org.iudx.adaptor.datatypes.Rule;
+import in.org.iudx.adaptor.datatypes.Message;
+import in.org.iudx.adaptor.utils.JsonFlatten;
+import in.org.iudx.adaptor.descriptors.RuleStateDescriptor;
+
+import org.apache.flink.runtime.state.PartitionableListState;
+import org.apache.flink.streaming.util.KeyedTwoInputStreamOperatorTestHarness;
+import org.apache.flink.streaming.util.TwoInputStreamOperatorTestHarness;
+import org.apache.flink.streaming.api.operators.co.CoBroadcastWithKeyedOperator;
+import org.apache.flink.util.Preconditions;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.streaming.api.operators.TwoInputStreamOperator;
+import org.apache.flink.streaming.api.functions.co.KeyedBroadcastProcessFunction;
+import org.apache.flink.runtime.checkpoint.OperatorSubtaskState;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
+import org.apache.flink.util.Collector;
+import org.apache.flink.streaming.util.AbstractStreamOperatorTestHarness;
+
+public class RuleFunctionTest {
+
+
+  private static TwoInputStreamOperatorTestHarness<Message,Rule,String> testHarness;
+
+
+  private static class IdentityKeySelector<T> implements KeySelector<T, T> {
+      private static final long serialVersionUID = 1L;
+
+      @Override
+      public T getKey(T value) throws Exception {
+          return value;
+      }
+  }
+
+
+  @BeforeAll
+  public static void initialize() throws Exception {
+
+    testHarness = getInitializedTestHarness(TypeInformation.of(String.class),
+                      (KeySelector<String,String>) new IdentityKeySelector<String>(),
+                      (KeyedBroadcastProcessFunction) new RuleFunction(),
+                       1, 1, 0, null);
+
+
+
+  }
+
+  @Test
+  public void prElem1() throws Exception {
+
+    Rule r1 = new Rule("select * from *");
+    r1.ruleId = 1;
+    testHarness.processWatermark2(new Watermark(5L));
+    testHarness.processElement2(r1, 10L);
+
+     Message m1 = new Message().setKey("b").setResponseBody("{\"a\": \"b\"}");
+     Message m2 = new Message().setKey("c").setResponseBody("{\"a\": \"c\"}");
+     testHarness.processWatermark1(new Watermark(20L));
+     testHarness.processElement1(new StreamRecord<Message>(m1, 30L));
+     testHarness.processWatermark1(new Watermark(10L));
+     testHarness.processElement1(new StreamRecord<Message>(m2, 20L));
+     System.out.println(testHarness.getOutput());
+
+    // Queue<Object> output = testHarness.getOutput();
+
+
+  }
+
+
+
+
+  private static <KEY, IN1, IN2, OUT>
+    TwoInputStreamOperatorTestHarness<IN1, IN2, OUT> getInitializedTestHarness(
+        final TypeInformation<KEY> keyTypeInfo,
+        final KeySelector<IN1, KEY> keyKeySelector,
+        final KeyedBroadcastProcessFunction<KEY, IN1, IN2, OUT> function,
+        final int maxParallelism,
+        final int numTasks,
+        final int taskIdx,
+        final OperatorSubtaskState initState)
+      throws Exception {
+
+      final TwoInputStreamOperatorTestHarness<IN1, IN2, OUT> testHarness =
+        new KeyedTwoInputStreamOperatorTestHarness<>(
+            new CoBroadcastWithKeyedOperator<>(
+              Preconditions.checkNotNull(function),
+              Collections.singletonList(RuleStateDescriptor.ruleMapStateDescriptor)),
+            keyKeySelector,
+            null,
+            keyTypeInfo,
+            maxParallelism,
+            numTasks,
+            taskIdx);
+
+      testHarness.setup();
+      testHarness.open();
+
+      return testHarness;
+        }
+
+
+}
