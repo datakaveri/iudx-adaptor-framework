aadcec5e091bd39d77a0dad2d1d08bb8c2b06644
Rakshit Ramesh <rakshitadmar@gmail.com>
2022-08-25 11:40:03 +0530

    [Upadte] RuleFunction flattens liststate elements

--- framework/src/main/java/in/org/iudx/adaptor/process/RuleFunction.java
+++ framework/src/main/java/in/org/iudx/adaptor/process/RuleFunction.java
@@ -17,6 +17,8 @@ import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.co.KeyedBroadcastProcessFunction;
 import org.apache.flink.util.Collector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -25,44 +27,53 @@ import java.sql.Statement;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
+import java.util.LinkedHashMap;
 
+
+import in.org.iudx.adaptor.utils.JsonFlatten;
 
 public class RuleFunction extends KeyedBroadcastProcessFunction<String, Message, Rule, String> {
 
-    private transient ListState<Message> listState;
-
-    private final ListStateDescriptor<Message> listStateDescriptor = new ListStateDescriptor<>("listState", TypeInformation.of(new TypeHint<>() {
+    private transient ListState<LinkedHashMap<String,Object>> listState;
+    private final ListStateDescriptor<LinkedHashMap<String,Object>> listStateDescriptor 
+        = new ListStateDescriptor<>("listState",
+                                    TypeInformation.of(
+                                      new TypeHint<LinkedHashMap<String,Object>>() {
     }));
 
     private transient CalciteConnection calciteConnection;
     @Override
     public void open(Configuration parameters) throws ClassNotFoundException, SQLException {
-        listState = getRuntimeContext().getListState(listStateDescriptor);
 
-        Class.forName("org.apache.calcite.jdbc.Driver");
 
         Properties info = new Properties();
         info.setProperty("lex", "JAVA");
         Connection connection = DriverManager.getConnection("jdbc:calcite:", info);
-
         calciteConnection = connection.unwrap(CalciteConnection.class);
+
+        listState = getRuntimeContext().getListState(listStateDescriptor);
 
     }
 
     @Override
-    public void processElement(Message message, KeyedBroadcastProcessFunction<String, Message, Rule, String>.ReadOnlyContext readOnlyContext, Collector<String> collector) throws Exception {
-        ReadOnlyBroadcastState<String, Rule> ruleState = readOnlyContext.getBroadcastState(RuleStateDescriptor.ruleMapStateDescriptor);
-        listState.add(message);
+    public void processElement(Message message, KeyedBroadcastProcessFunction<String,
+                  Message, Rule, String>.ReadOnlyContext readOnlyContext,
+                  Collector<String> collector) throws Exception {
+
+        ReadOnlyBroadcastState<String, Rule> ruleState 
+              = readOnlyContext.getBroadcastState(RuleStateDescriptor.ruleMapStateDescriptor);
+
+        listState.add(
+            (LinkedHashMap<String,Object>) new JsonFlatten(new ObjectMapper()
+                                                              .readTree(message.toString()))
+                                                          .flatten());
 
         SchemaPlus rootSchema = calciteConnection.getRootSchema();
-        Iterator<Message> iterator = listState.get().iterator();
-
-        List<Message> list = IteratorUtils.toList(iterator);
+        Iterator<LinkedHashMap<String,Object>> iterator = listState.get().iterator();
+        List<LinkedHashMap> list = IteratorUtils.toList(iterator);
 
         Schema schema = new Schema();
-
-        // TODO
-//        schema.setData(list);
+        schema.setData(list);
         rootSchema.add("listState", schema);
 
         try (Statement statement = calciteConnection.createStatement()) {
@@ -70,7 +81,7 @@ public class RuleFunction extends KeyedBroadcastProcessFunction<String, Message,
             ResultSet rs = statement.executeQuery(sql);
 
             while (rs.next()) {
-                String result = rs.getString("deviceId");
+                String result = rs.getString("index");
                 collector.collect(String.valueOf(result));
             }
         } catch (Exception e) {
@@ -79,8 +90,11 @@ public class RuleFunction extends KeyedBroadcastProcessFunction<String, Message,
     }
 
     @Override
-    public void processBroadcastElement(Rule rule, KeyedBroadcastProcessFunction<String, Message, Rule, String>.Context context, Collector<String> collector) throws Exception {
-        BroadcastState<String, Rule> broadcastState = context.getBroadcastState(RuleStateDescriptor.ruleMapStateDescriptor);
+    public void processBroadcastElement(Rule rule, KeyedBroadcastProcessFunction<String,
+                                        Message, Rule, String>.Context context,
+                                        Collector<String> collector) throws Exception {
+        BroadcastState<String, Rule> broadcastState 
+                  = context.getBroadcastState(RuleStateDescriptor.ruleMapStateDescriptor);
         broadcastState.put(rule.sqlQuery, rule);
     }
 }
