639eb0fe64544c1d78aaca90dff72c8cbdc72536
swarup-e <swarup@datakaveri.org>
2022-08-25 20:23:03 +0530

    [Update] WIP - added rule type and deserializer to parse rule source

--- framework/src/main/java/in/org/iudx/adaptor/datatypes/Rule.java
+++ framework/src/main/java/in/org/iudx/adaptor/datatypes/Rule.java
@@ -3,22 +3,90 @@ package in.org.iudx.adaptor.datatypes;
 import org.apache.flink.api.common.time.Time;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Rule implements Serializable {
 
-    public String sqlQuery;
     public Integer ruleId;
-    //TODO: SinkConfig here
-    public String sinkInfo;
-
+    public String sqlQuery;
+    public RuleType type;
     private Integer windowMinutes;
 
-    public Rule(String sqlQuery, String sinkInfo) {
+    public String sinkExchangeKey;
+    public String sinkRoutingKey;
+
+    public Rule(Integer ruleId, String sqlQuery, RuleType type, Integer windowMinutes, String sinkExchangeKey, String sinkRoutingKey) {
+        this.ruleId = ruleId;
         this.sqlQuery = sqlQuery;
-        this.sinkInfo = sinkInfo;
+        this.type = type;
+        this.windowMinutes = windowMinutes;
+        this.sinkExchangeKey = sinkExchangeKey;
+        this.sinkRoutingKey = sinkRoutingKey;
+    }
+
+    public Integer getRuleId() {
+        return ruleId;
+    }
+
+    public void setRuleId(Integer ruleId) {
+        this.ruleId = ruleId;
+    }
+
+    public String getSqlQuery() {
+        return sqlQuery;
+    }
+
+    public void setSqlQuery(String sqlQuery) {
+        this.sqlQuery = sqlQuery;
+    }
+
+    public RuleType getType() {
+        return type;
+    }
+
+    public void setType(RuleType type) {
+        this.type = type;
+    }
+
+    public void setWindowMinutes(Integer windowMinutes) {
+        this.windowMinutes = windowMinutes;
+    }
+
+    public String getSinkExchangeKey() {
+        return sinkExchangeKey;
+    }
+
+    public void setSinkExchangeKey(String sinkExchangeKey) {
+        this.sinkExchangeKey = sinkExchangeKey;
+    }
+
+    public String getSinkRoutingKey() {
+        return sinkRoutingKey;
+    }
+
+    public void setSinkRoutingKey(String sinkRoutingKey) {
+        this.sinkRoutingKey = sinkRoutingKey;
     }
 
     public Long getWindowMillis() {
         return Time.minutes(this.windowMinutes).toMilliseconds();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rule rule = (Rule) o;
+        return ruleId.equals(rule.ruleId) && Objects.equals(sqlQuery, rule.sqlQuery) && type == rule.type && Objects.equals(windowMinutes, rule.windowMinutes) && Objects.equals(sinkExchangeKey, rule.sinkExchangeKey) && Objects.equals(sinkRoutingKey, rule.sinkRoutingKey);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ruleId, sqlQuery, type, windowMinutes, sinkExchangeKey, sinkRoutingKey);
+    }
+
+    public enum RuleType {
+        RULE,
+        DELETE
     }
 }
--- framework/src/main/java/in/org/iudx/adaptor/process/RuleSourceDeSerializer.java
+++ framework/src/main/java/in/org/iudx/adaptor/process/RuleSourceDeSerializer.java
@@ -1,19 +1,20 @@
 package in.org.iudx.adaptor.process;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import in.org.iudx.adaptor.datatypes.Rule;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.util.Collector;
 
 
-public class RuleSourceDeSerializer  extends RichFlatMapFunction<String, Rule> {
-    @Override
-    public void flatMap(String value, Collector<Rule> out) throws Exception {
-//         System.out.println(value);
-//         try {
-//             Rule rule = new Rule(value);
-//             out.collect(rule);
-//         } catch (Exception e) {
-//             System.out.println("Failed parsing rule, dropping it:" + value);
-//         }
-     }
+public class RuleSourceDeSerializer extends RichFlatMapFunction<String, Rule> {
+  @Override
+  public void flatMap(String value, Collector<Rule> out) throws Exception {
+    try {
+      ObjectMapper mapper = new ObjectMapper();
+      Rule rule = mapper.readValue(value, Rule.class);
+      out.collect(rule);
+    } catch (Exception e) {
+      System.out.println("Failed parsing rule, dropping it:" + value);
+    }
+  }
 }
