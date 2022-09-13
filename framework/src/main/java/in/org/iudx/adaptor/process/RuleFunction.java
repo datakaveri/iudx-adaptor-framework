d6d4bb8397320019214a46358128c676b2d97f55
swarup-e <swarup@datakaveri.org>
2022-09-13 11:08:30 +0530

    [Update] - added logger and metrics for rule function

--- framework/src/main/java/in/org/iudx/adaptor/process/RuleFunction.java
+++ framework/src/main/java/in/org/iudx/adaptor/process/RuleFunction.java
@@ -5,15 +5,18 @@ import in.org.iudx.adaptor.datatypes.Message;
 import in.org.iudx.adaptor.datatypes.Rule;
 import in.org.iudx.adaptor.datatypes.RuleResult;
 import in.org.iudx.adaptor.descriptors.RuleStateDescriptor;
+import in.org.iudx.adaptor.logger.CustomLogger;
 import in.org.iudx.adaptor.sql.Schema;
 import in.org.iudx.adaptor.utils.JsonFlatten;
 import org.apache.calcite.jdbc.CalciteConnection;
 import org.apache.calcite.schema.SchemaPlus;
 import org.apache.commons.collections.IteratorUtils;
+import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.state.*;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.metrics.Counter;
 import org.apache.flink.streaming.api.functions.co.KeyedBroadcastProcessFunction;
 import org.apache.flink.util.Collector;
 import org.json.JSONArray;
@@ -31,8 +34,27 @@ public class RuleFunction extends KeyedBroadcastProcessFunction<String, Message,
 
   private boolean enabledProcessTimer = true;
 
+  transient CustomLogger logger;
+  private transient Counter elementCounter;
+  private transient Counter alertCounter;
+  private transient Counter ruleCounter;
+
   @Override
   public void open(Configuration parameters) throws ClassNotFoundException, SQLException {
+    ExecutionConfig.GlobalJobParameters params = getRuntimeContext().getExecutionConfig().getGlobalJobParameters();
+    String appName = params.toMap().get("appName");
+    logger = new CustomLogger(JSPathProcessFunction.class, appName);
+    this.elementCounter = getRuntimeContext()
+            .getMetricGroup()
+            .counter("RuleFunctionElementCounter");
+
+    this.alertCounter = getRuntimeContext()
+            .getMetricGroup()
+            .counter("RuleFunctionAlertCounter");
+
+    this.ruleCounter = getRuntimeContext()
+            .getMetricGroup()
+            .counter("RuleFunctionRuleCounter");
 
     Properties info = new Properties();
     info.setProperty("lex", "JAVA");
@@ -51,6 +73,8 @@ public class RuleFunction extends KeyedBroadcastProcessFunction<String, Message,
   public void processElement(Message message,
                              KeyedBroadcastProcessFunction<String, Message, Rule, RuleResult>.ReadOnlyContext readOnlyContext,
                              Collector<RuleResult> collector) throws Exception {
+    this.elementCounter.inc();
+    logger.debug("[event_key - " + message.key + "] Processing rule event");
     ReadOnlyBroadcastState<Integer, Rule> ruleState = readOnlyContext.getBroadcastState(
             RuleStateDescriptor.ruleMapStateDescriptor);
 
@@ -71,7 +95,6 @@ public class RuleFunction extends KeyedBroadcastProcessFunction<String, Message,
     Iterator<LinkedHashMap<String, Object>> iterator = listState.get().iterator();
     List<LinkedHashMap<String, Object>> list = IteratorUtils.toList(iterator);
 
-    System.out.println(list);
     Schema schema = new Schema();
     schema.setData(list);
     rootSchema.add("listState", schema);
@@ -94,8 +117,9 @@ public class RuleFunction extends KeyedBroadcastProcessFunction<String, Message,
         RuleResult ruleResult = new RuleResult(json.toString(), rule.getValue().sinkExchangeKey,
                 rule.getValue().sinkRoutingKey);
         collector.collect(ruleResult);
+        this.alertCounter.inc();
       } catch (Exception e) {
-        System.out.println(e);
+        logger.error("Error in executing query", e);
       }
     });
   }
@@ -105,6 +129,8 @@ public class RuleFunction extends KeyedBroadcastProcessFunction<String, Message,
                                       KeyedBroadcastProcessFunction<String, Message, Rule,
                                               RuleResult>.Context context,
                                       Collector<RuleResult> collector) throws Exception {
+    this.ruleCounter.inc();
+    logger.debug("[rule_id - " + rule.ruleId + "] Processing rule broadcast");
     BroadcastState<Integer, Rule> broadcastState = context.getBroadcastState(
             RuleStateDescriptor.ruleMapStateDescriptor);
     if (rule.type == Rule.RuleType.RULE) {
@@ -119,6 +145,7 @@ public class RuleFunction extends KeyedBroadcastProcessFunction<String, Message,
         try {
           updateExpiryTime(rule, broadcastState);
         } catch (Exception e) {
+          logger.error("Broadcast process error", e);
           throw new RuntimeException(e);
         }
       });
@@ -128,7 +155,7 @@ public class RuleFunction extends KeyedBroadcastProcessFunction<String, Message,
   @Override
   public void onTimer(final long timestamp, final OnTimerContext ctx,
                       final Collector<RuleResult> collector) throws Exception {
-    System.out.println("========Timer=======");
+    logger.debug("Running timer function for cleanup");
     Rule rule = ctx.getBroadcastState(RuleStateDescriptor.ruleMapStateDescriptor)
             .get(EXPIRY_TIME_RULE_ID);
 
@@ -152,6 +179,7 @@ public class RuleFunction extends KeyedBroadcastProcessFunction<String, Message,
 
   private void updateExpiryTime(Rule rule, BroadcastState<Integer, Rule> broadcastState)
           throws Exception {
+    logger.debug("Update widest expiry time");
     Rule widestWindowRule = broadcastState.get(EXPIRY_TIME_RULE_ID);
 
     if (widestWindowRule == null) {
@@ -168,23 +196,22 @@ public class RuleFunction extends KeyedBroadcastProcessFunction<String, Message,
 
   private void removeElementFromState(Long threshold) {
     enabledProcessTimer = true;
-    System.out.println("=======Removing element=======");
+    logger.debug("Cleanup expired list state elements");
     try {
       Iterator<LinkedHashMap<String, Object>> iterator = listState.get().iterator();
       while (iterator.hasNext()) {
         LinkedHashMap<String, Object> obj = iterator.next();
         Timestamp eventTimestamp = (Timestamp) obj.get("observationDateTime");
         long eventTime = eventTimestamp.toInstant().toEpochMilli();
-        System.out.println(eventTime);
-        System.out.println(threshold);
         if (eventTime < threshold) {
           iterator.remove();
         }
       }
       List<LinkedHashMap<String, Object>> list = IteratorUtils.toList(iterator);
       listState.update(list);
-    } catch (Exception ex) {
-      throw new RuntimeException(ex);
+    } catch (Exception e) {
+      logger.error("Error in removing element from state", e);
+      throw new RuntimeException(e);
     }
   }
 }
