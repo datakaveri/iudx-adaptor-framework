3bcb80701a8c6fcd218983809005328c9d602383
swarup-e <swarup@datakaveri.org>
2022-09-21 19:59:47 +0530

    [Update] - adaptor server build to latest

--- setup/build.sh
+++ setup/build.sh
@@ -4,7 +4,7 @@
 
 # Mock http server
 #docker build -t datakaveri/adaptor-mockserver:2.3.3 -f setup/mockserver/mockserver.dockerfile .
-docker build -t datakaveri/adaptor-server:2.3.3 -f setup/server/server.dockerfile .
+docker build -t datakaveri/adaptor-server:latest -f setup/server/server.dockerfile .
 
 
 #cd ./setup/flink/
