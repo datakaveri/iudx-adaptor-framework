40551796d1a30e2d7bb1ef90a31a84396c320159
Swarup E <swarup@datakaveri.org>
2023-04-12 07:20:41 +0000

    [Update] - namespace fix in flink start script

--- setup/k8s/flink/start.sh
+++ setup/k8s/flink/start.sh
@@ -1,6 +1,6 @@
 kubectl create namespace adaptor-framework
 kubectl create serviceaccount flink-service-account -n adaptor-framework
-kubectl create clusterrolebinding flink-role-binding-flink --clusterrole=edit --serviceaccount=default:flink-service-account -n adaptor-framework
+kubectl create clusterrolebinding flink-role-binding-flink --clusterrole=edit --serviceaccount=adaptor-framework:flink-service-account -n adaptor-framework
 ./bin/kubernetes-session.sh -Dkubernetes.cluster-id=flink-cluster -Dkubernetes.service-account=flink-service-account -Dkubernetes.container.image=datakaveri/flink:1.16.1
 kubectl apply -f flink-metrics-service.yaml
 kubectl apply -f taskmanager-metrics-service.yaml
